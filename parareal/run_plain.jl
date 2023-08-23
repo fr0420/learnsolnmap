cce7661a5f2a449454278f27a854024222ee3112
fr0420 <ruifangfr@gmail.com>
2023-08-23 16:01:37 -0500

    add command line argument for omega

--- parareal/run_plain.jl
+++ parareal/run_plain.jl
@@ -13,86 +13,80 @@ const fine_method = ARGS[8];
 const coarse_method = ARGS[9];
 const output_dir = ARGS[10];
 const num_workers = parse(Int, ARGS[11]);
-
-println("problem =       ", problem)
-println("T_init =        ", T_init)
-println("T_end =         ", T_end)
-println("N =             ", N)
-println("Nf =            ", Nf)
-println("Nc =            ", Nc)
-println("niters =        ", niters)
-println("fine method =   ", fine_method)
-println("coarse method = ", coarse_method)
-println("output_dir =    ", output_dir)
-println("# workers =     ", num_workers)
+const use_float64x4 = parse(Bool, ARGS[12]);
+const fpu_omega = parse(Float64, ARGS[13]);
 
 DeltaT = (T_end-T_init) / N
 deltat = DeltaT / Nf
 dT = DeltaT / Nc
 
+println("problem =         ", problem)
+println("fpu_omega =       ", fpu_omega)
+println("T_init =          ", T_init)
+println("T_end =           ", T_end)
+println("N =               ", N)
+println("Nf =              ", Nf)
+println("Nc =              ", Nc)
+println("niters =          ", niters)
+println("fine method =     ", fine_method)
+println("coarse method =   ", coarse_method)
 println("Delta T com =     ", DeltaT)
 println("fine stepsize =   ", deltat)
 println("coarse stepsize = ", dT)
+println("use_float64x4 =   ", use_float64x4)
+println("output_dir =      ", output_dir)
+println("# workers =       ", num_workers)
 
-
-using Distributed  # for parallel computing
-addprocs(num_workers);
-println("\nworkers: ", workers())
 
 include("utils.jl")
 include("Parareal.jl")
 using .Parareal
 using Dates
 using DataStructures
+using Distributed  # for parallel computing
+addprocs(num_workers);
+println("\nworkers: ", workers())
 
 
 config = OrderedDict(
-    "problem"=>problem, 
+    "problem"=>problem, "omega"=>fpu_omega,
     "T_init"=>T_init, "T_end"=>T_end, 
     "N"=>N, "Nf"=>Nf, "Nc"=>Nc, "niters"=>niters,
-    "fine method"=>fine_method, "coarse method"=>coarse_method, 
+    "fine method"=>fine_method, "coarse method"=>coarse_method, "use_float64x4"=>use_float64x4,
     "Delta T com"=>DeltaT, "fine stepsize"=>deltat, "coarse stepsize"=>dT
 )
 save_config(output_dir, config)
 
 
 @everywhere begin 
-    
-    include("./setups/$($problem).jl")
-    
-    using DifferentialEquations
-    
-    function ode_solve(A, method, p0, q0, t0, H, nsteps)
+    include("../tools/setups/$($problem).jl")
+    include("../tools/ode_solver.jl")
+    using ProgressMeter 
 
-        h = H/nsteps 
-        prob = SecondOrderODEProblem((du,u,p,t)->A(u), p0, q0, (t0, t0+H));
-        sol = solve(prob, method, tstops=t0:h:(t0+H), adaptive=false);
-        p = sol[end].x[1]
-        q = sol[end].x[2]
-
-        return p, q
-    end
-
-    methods = Dict(
-        "VelocityVerlet"=>VelocityVerlet(), 
-        "CalvoSanz4"=>CalvoSanz4()
-    )
+    if $problem == "fpu"
+        param = $use_float64x4 ? (Float64x4($fpu_omega)^2)/2. : ($fpu_omega^2)/2.
+    end 
     
-    fine_solve(p0, q0, t0, H) = ode_solve(A, methods[$fine_method], p0, q0, t0, H, $Nf)
-    coarse_solve(p0, q0, t0, H) = ode_solve(A, methods[$coarse_method], p0, q0, t0, H, $Nc)
-    
+    fine_solve(p0, q0, t0, H) = ode_solve(A!, methods[$fine_method], p0, q0, t0, H, $Nf, false, param)
+    coarse_solve(p0, q0, t0, H) = ode_solve(A!, methods[$coarse_method], p0, q0, t0, H, $Nc, false, param)
 end
 
 
-if problem == "lennardjones"
-    p0 = v0
-    q0 = x0
+if problem == "fpu"
+    kwargs = Dict(:omega => use_float64x4 ? Float64x4(fpu_omega) : fpu_omega)
 end 
 
-println("p0: $p0")
-println("q0: $q0")    
+p0, q0 = initial_condition(; kwargs...)
+
+println("\nInitial condition:")
+println("p0: ", p0)
+println("q0: ", q0)
+println("H0: ", compute_H(p0, q0; kwargs...))
+println("K0: ", compute_K(p0))
+println("U0: ", compute_U(q0; kwargs...))
 
 
+println("\nRunning plain parareal ...")
 t = collect(T_init:DeltaT:T_end)    
 p_all, q_all = Parareal.plain(p0, q0, t, fine_solve, coarse_solve, niters=niters)
 save_solutions(output_dir, t, p_all, q_all)
