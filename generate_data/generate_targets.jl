398652f2597ce40ca2b277baccf135fed49e2564
fr0420 <ruifangfr@gmail.com>
2023-08-23 16:11:36 -0500

    update data generation code

--- generate_data/generate_targets.jl
+++ generate_data/generate_targets.jl
@@ -1,75 +1,61 @@
+"""
+Generate targets 
+"""
+
+const problem = ARGS[1];
+const Dt = parse(Float64, ARGS[2]);
+const Nf = parse(Int64, ARGS[3]);
+const method = ARGS[4];
+const input_file = ARGS[5]
+const output_file = ARGS[6];
+const use_float64x4 = parse(Bool, ARGS[7]);
+
+println("problem =     ", problem)
+println("Delta t =     ", Dt)
+println("Nf =          ", Nf)
+println("h =           ", Dt/Nf)
+println("method =      ", method)
+println("input_file =  ", input_file)
+println("output_file = ", output_file)
+println("use_float64x4 =", use_float64x4)
+
+
 include("./generate_data_utils.jl")
 using Distributed 
 addprocs(40);
 println("# workers = 40")
+@everywhere using MultiFloats
 
-const problem = ARGS[1];
-const Dt = parse(Float64, ARGS[2]);
-const h = parse(Float64, ARGS[3]);
-const method = ARGS[4];
-const input_file = ARGS[5]
-const output_file = ARGS[6];
-
-println("problem =     ", problem)
-println("Delta t =     ", Dt)
-println("h =           ", h)
-println("method =      ", method)
-println("input_file =  ", input_file)
-println("output_file = ", output_file)
-
+if problem == "fpu"
+    const OMEGA = 50.
+    const param = use_float64x4 ? (Float64x4(OMEGA)^2)/2. : (OMEGA^2)/2.
+    
+end 
 
 @everywhere begin 
-    using DifferentialEquations
+    include("../tools/ode_solver.jl")
+    include("../tools/setups/$($problem).jl")
+    using ProgressMeter 
     
-    include("./setups/$($problem).jl")
-
-    function ode_solve(A, method, p0, q0, t0, H, nsteps, retfull)
-        
-        h = H/nsteps 
-        prob = SecondOrderODEProblem((du,u,p,t)->A(u), p0, q0, (t0, t0+H));
-        sol = solve(prob, method, tstops=t0:h:(t0+H), adaptive=false);
-        if retfull 
-            P = hcat([u.x[1] for u in sol.u]...)   
-            Q = hcat([u.x[2] for u in sol.u]...)
-            return P, Q
-        else 
-            p = sol[end].x[1]
-            q = sol[end].x[2]
-            return p, q
-        end
-    end
-    
-    const Dt = $Dt;
-    const h = $h;
-    
-    methods = Dict(
-        "VelocityVerlet"=>VelocityVerlet(), 
-        "CalvoSanz4"=>CalvoSanz4()
-    )
-    
-    phi_Dt(p, q) = ode_solve(A, methods[$method], p, q, 0.0, Dt, round(Int, Dt/h), false)
-    
+    phi_Dt(p, q) = ode_solve(A!, methods[$method], p, q, 0.0, $Dt, $Nf, false, $param)
 end
 
 
 
 println("\nImporting initial states ...")
 
-df_init = DataFrame(CSV.File(input_file));
-dim = div(size(df_init)[2], 2);
-P_init = Matrix(df_init[:, 1:dim])'; 
-Q_init = Matrix(df_init[:, dim+1:end])';
+P_init, Q_init = read_csv(input_file, use_float64x4 ? Float64x4 : Float64)
 
-println("Done.")
+println(size(P_init)[2], " states imported.")
 
 
 println("\nComputing final states ...")
 
-res = pmap(phi_Dt, eachslice(P_init, dims=2), eachslice(Q_init, dims=2));
-P_final = hcat([p for (p, q) in res]...);
-Q_final = hcat([q for (p, q) in res]...);
+res = @showprogress pmap(phi_Dt, eachslice(P_init, dims=2), eachslice(Q_init, dims=2))
+P_final = hcat([p for (p, q) in res]...)
+Q_final = hcat([q for (p, q) in res]...)
 
 println("Done.")
 
-save(output_file, P_final, Q_final);
+save_csv(output_file, P_final, Q_final, use_float64x4 ? Float64x4 : Float64)
 
