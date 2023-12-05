abea26fd1a484ef0a8049d00abcd2722b3a71dff
fr0420 <ruifangfr@gmail.com>
2023-12-05 10:22:00 -0600

    add ResMLP2 class

--- deep_learning/model.py
+++ deep_learning/model.py
@@ -192,7 +192,52 @@ class ResMLP(nn.Module):
             return x, hs 
         else:         
             return x
+
+
+class ResMLP2(nn.Module):
+    """Multi-layer perceptron with residual connections between layers of equal width"""
     
+    def __init__(self, layer_sizes, activation_fn, activation_kwargs, use_bn, use_scale):
+        super(ResMLP2, self).__init__()
+        
+        self.layer_sizes = layer_sizes
+        self.layers = nn.ModuleList(
+            [nn.Linear(self.layer_sizes[i], self.layer_sizes[i+1]) for i in range(len(self.layer_sizes)-1)]
+        )
+        self.activation = get_activation(activation_fn, **activation_kwargs) 
+        self.use_bn = use_bn
+        if self.use_bn:
+            self.bn_layers = nn.ModuleList(
+                [nn.BatchNorm1d(self.layer_sizes[i+1]) for i in range(len(self.layer_sizes)-2)]
+            )
+        self.scale = 1. / len(self.layers) if use_scale else 1.
+        
+    def forward(self, x, return_hidden=False):
+        init_x = x
+        hs = [] 
+        for i, layer in enumerate(self.layers[:-1]):
+            identity = x 
+            x = layer(x)
+            if self.use_bn:
+                x = self.bn_layers[i](x)
+            x = self.activation(x)
+            if layer.in_features == layer.out_features:
+                x = identity + self.scale * x
+            if return_hidden: 
+                hs.append(x) 
+        
+        output_layer = self.layers[-1]
+        identity = x
+        x = output_layer(x)
+        if output_layer.in_features == output_layer.out_features:
+            x = self.activation(x)
+            x = identity + self.scale * x
+
+        if return_hidden:
+            return init_x + x, hs 
+        else:
+            return init_x + x
+        
 
 class HamiltonianReversibleBlock(nn.Module):
     """Hamiltonian Reversible Block"""
@@ -304,10 +349,11 @@ def get_model(model_name, layer_sizes, activation_fn, activation_kwargs, use_bn,
     
     model_fn = {
         "MLP": MLP, 
-        "ResMLP": ResMLP, 
+        "ResMLP": ResMLP,
+        "ResMLP2": ResMLP2,
         "HamiltonianReversibleNetwork": HamiltonianReversibleNetwork
     }[model_name]
-    if model_name == "ResMLP":
+    if model_name == "ResMLP" or model_name == "ResMLP2":
         return model_fn(layer_sizes, activation_fn, activation_kwargs, use_bn, use_scale)
     else:
         return model_fn(layer_sizes, activation_fn, activation_kwargs, use_bn)
