a0b83b4e6272b5845609d6224922912f3cbda49e
fr0420 <ruifangfr@gmail.com>
2023-12-15 02:31:14 -0600

    add equivariance network

--- deep_learning/model.py
+++ deep_learning/model.py
@@ -140,14 +140,21 @@ class MLP(nn.Module):
                 [nn.BatchNorm1d(self.layer_sizes[i+1]) for i in range(len(self.layer_sizes)-2)]
             )
         
-    def forward(self, x):
+    def forward(self, x, return_hidden=False):
+        hs = []
         for i in range(len(self.layers)-1):
             x = self.layers[i](x)
             if self.use_bn:
                 x = self.bn_layers[i](x)
             x = self.activation(x)
+            if return_hidden:
+                hs.append(x)
         x = self.layers[-1](x)
-        return x
+
+        if return_hidden:
+            return x, hs 
+        else:         
+            return x
 
 
 class ResMLP(nn.Module):
@@ -362,6 +369,47 @@ class HamiltonianReversibleNetwork(nn.Module):
                         s += torch.linalg.norm(layer_next.layer2.weight - layer_cur.layer2.weight) 
         return s / 1e-3
 
+
+class EquivarianceLayer(nn.Module):
+    """Equivariance Layer"""
+
+    def __init__(self, N, n_hidden_nodes, n_hidden_layers, activation_fn, activation_kwargs, use_bn):
+        super(EquivarianceLayer, self).__init__()
+
+        n_in = (2*N)**2
+        n_out = 2*N
+        self.mlp = MLP([n_in]+[n_hidden_nodes]*n_hidden_layers+[n_out], activation_fn, activation_kwargs, use_bn)
+
+    def forward(self, x):
+        scalars, basis = x
+        coeffs = self.mlp(scalars)  # batch_size x 2N
+        output = torch.bmm(coeffs.unsqueeze(1), basis).squeeze(1)  # batch_size x d
+        return output
+
+
+class EquivarianceNetwork(nn.Module):
+    """Equivariance Network"""
+
+    def __init__(self, N, d, n_hidden_nodes, n_hidden_layers, activation_fn, activation_kwargs, use_bn):
+        super(EquivarianceNetwork, self).__init__()
+
+        self.N = N
+        self.d = d
+        self.layers = nn.ModuleList(
+            EquivarianceLayer(N, n_hidden_nodes, n_hidden_layers, activation_fn, activation_kwargs, use_bn) for _ in range(2*N)
+        )
+        
+    def forward(self, x, return_hidden=False):
+        # x has shape (batch_size x 2Nd)
+        x = x.reshape(-1, 2*self.N, self.d)  # batch_size x 2N x d
+        scalars = torch.bmm(x, torch.transpose(x, 1, 2))  # batch_size x 2N x 2N
+        scalars = scalars.flatten(start_dim=1)  # batch_size x (2N)^2
+        output = torch.cat([l((scalars, x)) for l in self.layers], 1)  # batch_size x 2Nd
+        if return_hidden:
+            return output, scalars
+        else:
+            return output
+            
     
 class FrictionBlock(nn.Module):
     """Friction Block"""
@@ -401,10 +449,13 @@ def get_model(model_name, layer_sizes, activation_fn, activation_kwargs, use_bn,
         "ResMLP": ResMLP,
         "ResMLP2": ResMLP2,
         "ResMLP3": ResMLP3,
-        "HamiltonianReversibleNetwork": HamiltonianReversibleNetwork
+        "HamiltonianReversibleNetwork": HamiltonianReversibleNetwork,
+        "EquivarianceNetwork": EquivarianceNetwork
     }[model_name]
     if "ResMLP" in model_name:
         return model_fn(layer_sizes, activation_fn, activation_kwargs, use_bn, use_scale)
+    elif model_name == "EquivarianceNetwork":
+        return model_fn(7, 2, 200, 3, activation_fn, activation_kwargs, use_bn)
     else:
         return model_fn(layer_sizes, activation_fn, activation_kwargs, use_bn)
 
