c564dd19578e9806dafa304006947fbe842d7d5d
fr0420 <ruifangfr@gmail.com>
2024-01-24 12:54:56 -0600

    add tiramisu and symmetric resnet

--- deep_learning/src/networks/resnet.py
+++ deep_learning/src/networks/resnet.py
@@ -3,7 +3,7 @@ from torch import nn
 
 
 class ResidualBlock(nn.Module):
-    """A residual block."""
+    """A residual block with equal input and output features."""
 
     def __init__(self, n_features, activation, n_linears, scaling_factor, use_bn):
         super(ResidualBlock, self).__init__()
@@ -24,7 +24,7 @@ class ResidualBlock(nn.Module):
     
 
 class PostActivationResidualBlock(ResidualBlock):
-    """A post-activation residual block with [Linear, Batch Norm, Activation] * n, Addition."""
+    """A post-activation residual block: [Linear, Batch Norm, Activation] * n, Addition."""
 
     def __init__(self, n_features, activation, n_linears=1, scaling_factor=1., use_bn=False):
         super(PostActivationResidualBlock, self).__init__(
@@ -46,7 +46,7 @@ class PostActivationResidualBlock(ResidualBlock):
 
 
 class PreActivationResidualBlock(ResidualBlock):
-    """A pre-activation residual block with [Batch Norm, Activation, Linear] * n, Addition."""
+    """A pre-activation residual block: [Batch Norm, Activation, Linear] * n, Addition."""
 
     def __init__(self, n_features, activation, n_linears=1, scaling_factor=1., use_bn=False):
         super(PreActivationResidualBlock, self).__init__(
@@ -117,6 +117,111 @@ class ResNet(nn.Module):
         else:         
             return x
 
+
+class Block(nn.Module):
+    """A plain block."""
+
+    def __init__(self, in_features, out_features, activation, n_linears, use_bn, final=False):
+        super(Block, self).__init__()
+        
+        self.linears = nn.ModuleList([nn.Linear(in_features, out_features)])
+        for _ in range(n_linears-1):
+            self.linears.append(nn.Linear(out_features, out_features))
+        self.activation = activation
+        self.use_bn = use_bn
+        if self.use_bn:
+            self.bn_layers = nn.ModuleList(
+                [nn.BatchNorm1d(out_features) for _ in range(n_linears)]
+            )
+        self.final = final
+
+    def forward(self, x):
+        
+        if not self.final:
+            for i in range(len(self.linears)):
+                x = self.linears[i](x)
+                if self.use_bn:
+                    x = self.bn_layers[i](x)
+                x = self.activation(x)
+        
+        else:
+            for i in range(len(self.linears)-1):
+                x = self.linears[i](x)
+                if self.use_bn:
+                    x = self.bn_layers[i](x)
+                x = self.activation(x)
+            x = self.linears[-1](x)
+    
+        return x
+
+
+class SymmetricResNet(nn.Module):
+    """Symmetric residual network."""
+    
+    def __init__(self, input_dim, activation, expand_blocks_dims=(100, 200, 500),
+                 n_linears_per_block=3, use_bn=False):
+        super(SymmetricResNet, self).__init__()
+
+        blocks_dims = list(expand_blocks_dims) + list(expand_blocks_dims[::-1])
+        n_expand_blocks = n_shrink_blocks = len(expand_blocks_dims)
+        
+        if type(n_linears_per_block) == list or type(n_linears_per_block) == tuple:
+            assert (len(n_linears_per_block) == len(blocks_dims) + 1)
+        elif type(n_linears_per_block) == int:
+            n_linears_per_block = [n_linears_per_block] * (len(blocks_dims) + 1)
+        else:
+            raise ValueError
+        
+        ##################
+        # Expanding path #
+        ##################
+
+        n_in = input_dim
+        self.expand_blocks = nn.ModuleList([])
+        for i in range(n_expand_blocks):
+            n_out = blocks_dims[i]
+            self.expand_blocks.append(
+                Block(n_in, n_out, activation, n_linears_per_block[i], use_bn)
+            )
+            n_in = n_out
+
+        #####################
+        #     Bottleneck    #
+        #####################
+
+        self.bottleneck = Block(n_in, n_out, activation, n_linears_per_block[n_expand_blocks], use_bn)
+
+        #######################
+        #   Shrinking path   #
+        #######################
+
+        self.shrink_blocks = nn.ModuleList([])
+        for i in range(n_expand_blocks+1, n_expand_blocks+n_shrink_blocks):
+            n_out = blocks_dims[i]
+            self.shrink_blocks.append(
+                Block(n_in, n_out, activation, n_linears_per_block[i], use_bn)
+            )
+            n_in = n_out
+        n_out = input_dim
+        self.shrink_blocks.append(
+            Block(n_in, n_out, activation, n_linears_per_block[-1], use_bn, final=True)
+        )
+
+    def forward(self, x):
+        
+        skip_connections = []
+        for block in self.expand_blocks:
+            x = block(x)
+            skip_connections.append(x)
+
+        x = self.bottleneck(x)
+        for block in self.shrink_blocks:
+            skip = skip_connections.pop()
+            x += skip
+            x = block(x)
+        
+        return x
+        
 
 class ResMLP(nn.Module):
     """Multi-layer perceptron with residual connections between layers of equal width."""
