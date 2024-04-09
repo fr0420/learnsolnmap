0d11c671d514f66a1dc079f36faf3f27dcca8d1f
fr0420 <ruifangfr@gmail.com>
2024-04-09 14:47:50 -0500

    minor changes

--- bash_scripts/generate_data.sh
+++ bash_scripts/generate_data.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 PROJECT_ROOT=/workspace/projects_rui/learnsolnmap
-DATA_DIR=$PROJECT_ROOT/data1/fpu/omega=300/version0
+PROBLEM=fpu
+DATA_DIR=$PROJECT_ROOT/data1/$PROBLEM/omega=300/version0
 DIR_INPUTS=$DATA_DIR/inputs/
-DIR_TARGETS=$DATA_DIR/Dt=1e0_v1/
+DIR_TARGETS=$DATA_DIR/Dt=1e0_v3/
 
 
 # generate inputs 
-julia src/data/generate_inputs.jl configs/fpu/inputs.toml --output_dir $DIR_INPUTS
+# julia src/data/generate_inputs.jl configs/$PROBLEM/inputs.toml --output_dir $DIR_INPUTS &&
 
 # generate targets
 mkdir -p $DIR_TARGETS
 cp $DIR_INPUTS/U0.csv $DIR_TARGETS
-julia src/data/generate_targets.jl configs/fpu/targets.toml --output_dir $DIR_TARGETS
+julia src/data/generate_targets.jl configs/$PROBLEM/targets.toml --output_dir $DIR_TARGETS &&
 
 # split into train/test sets
 python3 deep_learning/src/split_data.py --data_dir $DIR_TARGETS
--- bash_scripts/run_parareal.sh
+++ bash_scripts/run_parareal.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 PROJECT_ROOT=/workspace/projects_rui/learnsolnmap
-OUTPUT_DIR=$PROJECT_ROOT/out/fpu/omega=300/202403072132
+PROBLEM=argoncrystal
+OUTPUT_DIR=$PROJECT_ROOT/out/$PROBLEM/202403181802
 
-julia src/parareal/run_parareal.jl configs/fpu/parareal_sequential.toml --output_dir $OUTPUT_DIR/ref
-julia src/parareal/run_parareal.jl configs/fpu/parareal_plain.toml --output_dir $OUTPUT_DIR/plain
-julia src/parareal/run_parareal.jl configs/fpu/parareal_procrustes.toml --output_dir $OUTPUT_DIR/procrustes
-julia src/parareal/run_parareal.jl configs/fpu/parareal_interpolative.toml --output_dir $OUTPUT_DIR/interpolative
+# julia src/parareal/run_parareal.jl configs/$PROBLEM/parareal_sequential.toml --output_dir $OUTPUT_DIR/ref
+# julia src/parareal/run_parareal.jl configs/$PROBLEM/parareal_plain.toml --output_dir $OUTPUT_DIR/plain
+# julia src/parareal/run_parareal.jl configs/$PROBLEM/parareal_procrustes.toml --output_dir $OUTPUT_DIR/procrustes
+julia src/parareal/run_parareal.jl configs/$PROBLEM/parareal_interpolative.toml --output_dir $OUTPUT_DIR/interpolative2
