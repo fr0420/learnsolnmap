8a7ff199d8c9cc160e18340f305a8dfd93624b9c
fr0420 <ruifangfr@gmail.com>
2024-07-24 11:53:40 -0500

    update configs and bash scripts related to data generation

--- bash_scripts/generate_data.sh
+++ bash_scripts/generate_data.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 PROJECT_ROOT=/workspace/projects_rui/learnsolnmap
-PROBLEM=fpu
-DATA_DIR=$PROJECT_ROOT/data1/$PROBLEM/omega=300/version0
+PROBLEM=nco
+DATA_DIR=$PROJECT_ROOT/data1/$PROBLEM/epsilon=1e-2/version22
 DIR_INPUTS=$DATA_DIR/inputs/
-DIR_TARGETS=$DATA_DIR/Dt=1e0_v3/
-
+DIR_TARGETS=$DATA_DIR/Dt=5e0/
 
 # generate inputs 
-# julia src/data/generate_inputs.jl configs/$PROBLEM/inputs.toml --output_dir $DIR_INPUTS &&
+# julia src/data_generation/generate_inputs.jl configs/$PROBLEM/inputs.toml --output_dir $DIR_INPUTS &&
 
 # generate targets
 mkdir -p $DIR_TARGETS
 cp $DIR_INPUTS/U0.csv $DIR_TARGETS
-julia src/data/generate_targets.jl configs/$PROBLEM/targets.toml --output_dir $DIR_TARGETS &&
+julia src/data_generation/generate_targets.jl configs/$PROBLEM/targets.toml --output_dir $DIR_TARGETS 
 
 # split into train/test sets
 python3 deep_learning/src/split_data.py --data_dir $DIR_TARGETS
